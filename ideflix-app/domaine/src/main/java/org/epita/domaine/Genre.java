c6380aec324b4c746b2867c6f4175a688476a9f7
Nicolas Mauge <36577679+NicolasMauge@users.noreply.github.com>
2023-07-10 16:05:20 +0200

    - Ajout de la classe Genre dans domaine - Correction dans les versions sur le pom parent

---
+++ ideflix-app/domaine/src/main/java/org/epita/domaine/Genre.java
@@ -0,0 +1,44 @@
+package org.epita.domaine;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String idTmdb;
+
+    private String nomGenre;
+
+    public Genre() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIdTmdb() {
+        return idTmdb;
+    }
+
+    public void setIdTmdb(String idTmdb) {
+        this.idTmdb = idTmdb;
+    }
+
+    public String getNomGenre() {
+        return nomGenre;
+    }
+
+    public void setNomGenre(String nomGenre) {
+        this.nomGenre = nomGenre;
+    }
+}
