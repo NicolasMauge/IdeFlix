893397e8bd389ffdefacd51185142e3e39c8c480
Stéphane Eveillé <stephane.eveille-epita@outlook.fr>
2023-07-11 16:18:11 +0200

    Création projet IAM

---
+++ ideflix-iam/exposition/src/main/java/org/epita/ideflixiam/IdeflixIamMain.java
@@ -0,0 +1,20 @@
+package org.epita.ideflixiam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeflixIamMain {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(IdeflixIamMain.class, args);
+
+        System.out.println("========================================================================================");
+        System.out.println(" Démarrage Ideflix IAM");
+        System.out.println("========================================================================================");
+        System.out.println("");
+
+
+    }
+}
