a64b5fe890788a97484c5d30589d220901a3d575
Nicolas Mauge <36577679+NicolasMauge@users.noreply.github.com>
2023-07-11 09:41:39 +0200

    - pour les tests : * modification du pom de exposition (mais il n'est pas complet) * ajout d'une dépendance application dans le pom exposition * ajout d'une dépendance infrastructure dans le pom exposition * ajout d'une dépendance domaine dans le pom application * ajout du sous-module infrastructure (pour dépendance postgres) * ajout de la dépendance postgres dans infrastructure

--- ideflix-app/application/src/main/java/org/epita/application/GenreService.java
+++ ideflix-app/application/src/main/java/org/epita/application/GenreService.java
@@ -1,5 +1,7 @@
 package org.epita.application;
 
+import org.epita.domaine.media.Genre;
+
 public interface GenreService {
 
     Genre creerGenre(Genre nouveauGenre);
--- ideflix-app/exposition/src/main/java/org/epita/Main.java
+++
@@ -1,7 +0,0 @@
-package org.epita;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
