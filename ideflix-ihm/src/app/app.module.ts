9e708d7de07a7a813d9f58365d8aa8bf4932e2ba
Caroline Lambert <caroline.lambert@epita.fr>
2023-07-12 18:02:53 +0200

    ajout de login et register

--- ideflix-ihm/src/app/app.module.ts
+++ ideflix-ihm/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AccueilComponent } from './accueil/accueil.component';
 import {AppRoutingModule} from "./app-routing.module";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import {MatSnackBarModule} from "@angular/material/snack-bar";
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MatSnackBarModule
   ],
