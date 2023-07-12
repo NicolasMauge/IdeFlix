5a2e2c54295b6ecf1d0a63df0bdaa1e6d37c124f
Caroline Lambert <caroline.lambert@epita.fr>
2023-07-12 15:25:46 +0200

    création page d'accueil

--- ideflix-ihm/src/app/app.module.ts
+++ ideflix-ihm/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule
