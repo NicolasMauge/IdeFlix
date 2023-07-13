c3cc19648d389f1462218b8d40e2a6158e3362a4
Nicolas Mauge <36577679+NicolasMauge@users.noreply.github.com>
2023-07-13 17:15:18 +0200

    APP : - corrections sur PréférencesUtilisateursMapper

--- ideflix-app/exposition/src/main/java/org/epita/exposition/utilisateur/preferences/PreferencesUtilisateurMapper.java
+++ ideflix-app/exposition/src/main/java/org/epita/exposition/utilisateur/preferences/PreferencesUtilisateurMapper.java
@@ -1,5 +1,6 @@
 package org.epita.exposition.utilisateur.preferences;
 
+import org.epita.application.utilisateur.preferences.PreferencesUtilisateurService;
 import org.epita.domaine.utilisateur.PreferencesUtilisateurEntity;
 import org.epita.exposition.common.Mapper;
 import org.epita.exposition.media.genre.GenreMapper;
@@ -7,10 +8,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PreferencesUtilisateurMapper extends Mapper<PreferencesUtilisateurEntity, PreferencesUtilisateurDto> {
-    GenreMapper genreMapper;
+    private GenreMapper genreMapper;
+    private PreferencesUtilisateurService preferencesUtilisateurService;
 
-    public PreferencesUtilisateurMapper(GenreMapper genreMapper) {
+    public PreferencesUtilisateurMapper(GenreMapper genreMapper, PreferencesUtilisateurService preferencesUtilisateurService) {
         this.genreMapper = genreMapper;
+        this.preferencesUtilisateurService = preferencesUtilisateurService;
     }
 
     @Override
@@ -23,6 +26,7 @@ public class PreferencesUtilisateurMapper extends Mapper<PreferencesUtilisateurE
 
     @Override
     public PreferencesUtilisateurEntity mapDtoToEntity(PreferencesUtilisateurDto input) {
+        // TODO : attention, si on convertit un dto qui n'est pas complètement rempli, les valeurs nulls vont écraser les valeurs en base
         return new PreferencesUtilisateurEntity(
                 input.getId(),
                 input.getPseudo(),
