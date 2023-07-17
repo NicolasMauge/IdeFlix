3e81d1b5961da0decec2538edd96f57a1ea1ba89
Stéphane Eveillé <stephane.eveille-epita@outlook.fr>
2023-07-17 16:48:12 +0200

    IAM-Début mise en place test et ajout exception indiquant que l'utilisateur existe déjà lors de la création pour des raisons pratiques.

---
+++ ideflix-iam/application/src/test/java/org/epita/ideflixiam/application/utilisateur/UtilisateurServiceTest.java
@@ -0,0 +1,23 @@
+package org.epita.ideflixiam.application.utilisateur;
+
+import org.epita.ideflixiam.domaine.RoleEntity;
+import org.epita.ideflixiam.domaine.UtilisateurEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.epita.ideflixiam.application.common.UtileRole.ROLE_ADMIN;
+import static org.epita.ideflixiam.application.common.UtileRole.ROLE_UTILISATEUR;
+
+public class UtilisateurServiceTest {
+
+//    List<RoleEntity> roleEntityList = ArrraROLE_ADMIN, ROLE_UTILISATEUR});
+//
+//    private UtilisateurEntity utilisateurStandard = new UtilisateurEntity("Test",
+//            "Standard",
+//            "standard.test@example.org",
+//            "123456789",
+//            ())
+//
+
+}
