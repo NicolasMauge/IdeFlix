4fb272a1d3a02d768f6fb972389e0064e7f0b046
Nicolas Mauge <36577679+NicolasMauge@users.noreply.github.com>
2023-08-21 15:51:12 +0200

    IHM: mis en place des boutons en fonction de l'existence ou non du film / serie sélectionnée dans la liste

--- ideflix-ihm/src/app/selection-media/shared/services/media-selectionne-to-app.service.ts
+++ ideflix-ihm/src/app/selection-media/shared/services/media-selectionne-to-app.service.ts
@@ -2,16 +2,19 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {MediaAppModel} from "../../../core/models/media-app.model";
 import {MediaSelectionneModel} from "../model/MediaSelectionneModel";
-import {map} from "rxjs";
+import {BehaviorSubject, map, Observable} from "rxjs";
 import {EtiquetteModel} from "../model/EtiquetteModel";
 import {MediaService} from "../../../core/services/media/media.service";
 import {environment} from "../../../../environments/environment";
+import {MediaMaListeModel} from "../../../ma-liste-de-selection/models/media-ma-liste.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MediaSelectionneToAppService {
   IDEFLIX_API = environment.IDEFLIX_SERVER;
+  private _mediaSelectionne$ = new BehaviorSubject(<MediaMaListeModel[]>([]));
+
 
   constructor(private http: HttpClient) { }
 
@@ -19,5 +22,28 @@ export class MediaSelectionneToAppService {
     let endpoint = '/mediaselectionne';
 
     this.http.post(this.IDEFLIX_API + endpoint, media, {responseType: 'text'}).subscribe();
+  }
+
+  getMediaSelectionnePourEmailEtIdTmdb(): MediaMaListeModel[] {
+    return this._mediaSelectionne$.getValue();
+  }
+
+  // getter setter
+  get mediaSelectionne$():Observable<MediaMaListeModel[]> {
+    //asObservable pour que personne ne puisse faire un next là-dessus. Avec un observable, on ne peut que souscrire.
+    return this._mediaSelectionne$.asObservable();
+  }
+
+  trouveMediaSelectionnePourEmailEtIdTmdb(email: string, idTmdb: string) {
+    let endpoint = '/mediaselectionne/utilisateur/'+email+"/idtmdb/"+idTmdb;
+
+    this.http.get<String[]>(this.IDEFLIX_API + endpoint)
+      .pipe(
+        map(
+          (listMediaSelectionneApi: any) => listMediaSelectionneApi.map(
+            (mediaSelectionneApi: any) => new MediaMaListeModel(mediaSelectionneApi)
+        ))
+      )
+      .subscribe((data: MediaMaListeModel[])=> this._mediaSelectionne$.next(data));
   }
 }
