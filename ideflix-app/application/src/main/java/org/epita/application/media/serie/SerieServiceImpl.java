215198ecc83f6622e1896bfe829763d1bd5f2e17
Nicolas Mauge <36577679+NicolasMauge@users.noreply.github.com>
2023-08-22 18:35:56 +0200

    APP: mise à jour du service serie pour qu'il soit en phase avec le service film

--- ideflix-app/application/src/main/java/org/epita/application/media/serie/SerieServiceImpl.java
+++ ideflix-app/application/src/main/java/org/epita/application/media/serie/SerieServiceImpl.java
@@ -1,9 +1,14 @@
 package org.epita.application.media.serie;
 
+import org.epita.application.media.genre.GenreServiceImpl;
 import org.epita.domaine.common.EntityNotFoundException;
 import org.epita.domaine.media.FilmEntity;
+import org.epita.domaine.media.GenreEntity;
 import org.epita.domaine.media.SerieEntity;
+import org.epita.infrastructure.media.GenreRepository;
 import org.epita.infrastructure.media.SerieRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,14 +16,39 @@ import java.util.Optional;
 
 @Service
 public class SerieServiceImpl implements SerieService {
-    private SerieRepository serieRepository;
+    public static final Logger logger = LoggerFactory.getLogger(GenreServiceImpl.class);
 
-    public SerieServiceImpl(SerieRepository serieRepository) {
+    private SerieRepository serieRepository;
+    private GenreRepository genreRepository;
+
+    public SerieServiceImpl(SerieRepository serieRepository, GenreRepository genreRepository) {
         this.serieRepository = serieRepository;
+        this.genreRepository = genreRepository;
     }
 
     @Override
     public void creerSerie(SerieEntity serieEntity) {
+        // compléter les id des genres
+        serieEntity.getGenreList().forEach(genre-> {
+            Optional<GenreEntity> genreTrouve = this.genreRepository.findGenreEntityByIdTmdb(genre.getIdTmdb());
+            if(genreTrouve.isPresent()) {
+                genre.setId(genreTrouve.get().getId());
+                logger.debug("IdeFlix - creerSerie - Genre déjà existant : {id : "+genre.getId()+", idTmdb : " + genre.getIdTmdb() + ", nomGenre : " +genre.getNomGenre()+"}");
+            }
+            else {
+                logger.debug("IdeFlix - creerSerie - Problème : un genre n'existe pas, il faut d'abord le créer");
+            }
+        });
+
+        // on vérifie si le film existe déjà en base, et si oui, on ajoute son id trouvé
+        Optional<SerieEntity> serieTrouve = this.serieRepository.findByIdTmdb(serieEntity.getIdTmdb());
+        if(serieTrouve.isPresent()) {
+            serieEntity.setId(serieTrouve.get().getId());
+            logger.debug("IdeFlix - creerSerie : série trouvée avec l'idTmdb = " + serieEntity.getIdTmdb());
+        }
+
+        logger.debug("IdeFlix - creerSerie : tentative de création avec l'idTmdb = " + serieEntity.getIdTmdb());
+
         this.serieRepository.save(serieEntity);
     }
 
