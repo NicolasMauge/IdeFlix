e7574c2f3444847c30d052067929288ed0db2945
Stéphane Eveillé <stephane.eveille-epita@outlook.fr>
2023-08-24 17:13:46 +0200

    APP : ajouté : MediaSelectionneController.trouverTousLesGenresParUtilisateur

--- ideflix-app/exposition/src/main/java/org/epita/exposition/controller/selection/MediaSelectionneController.java
+++ ideflix-app/exposition/src/main/java/org/epita/exposition/controller/selection/MediaSelectionneController.java
@@ -2,45 +2,52 @@ package org.epita.exposition.controller.selection;
 
 import org.epita.application.selection.filmselectionne.FilmSelectionneService;
 import org.epita.application.selection.serieselectionnee.SerieSelectionneeService;
+import org.epita.domaine.media.GenreEntity;
 import org.epita.domaine.selection.FilmSelectionneEntity;
+import org.epita.domaine.selection.MediaSelectionneEntity;
 import org.epita.domaine.selection.SerieSelectionneeEntity;
 import org.epita.exposition.common.Mapper;
+import org.epita.exposition.controller.media.GenreController;
 import org.epita.exposition.dto.common.TypeMedia;
+import org.epita.exposition.dto.media.GenreDto;
 import org.epita.exposition.dto.selection.MediaSelectionneCompletDto;
 import org.epita.exposition.dto.selection.MediaSelectionneDto;
+import org.epita.exposition.mapper.media.genre.GenreMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("/mediaselectionne")
 public class MediaSelectionneController {
-    private FilmSelectionneService filmSelectionneService;
-    private SerieSelectionneeService serieSelectionneeService;
     Mapper<FilmSelectionneEntity, MediaSelectionneDto> filmMapper;
     Mapper<FilmSelectionneEntity, MediaSelectionneCompletDto> filmCompletMapper;
     Mapper<SerieSelectionneeEntity, MediaSelectionneDto> serieMapper;
     Mapper<SerieSelectionneeEntity, MediaSelectionneCompletDto> serieCompletMapper;
 
-    public MediaSelectionneController(FilmSelectionneService filmSelectionneService, SerieSelectionneeService serieSelectionneeService, Mapper<FilmSelectionneEntity, MediaSelectionneDto> filmMapper, Mapper<FilmSelectionneEntity, MediaSelectionneCompletDto> filmCompletMapper, Mapper<SerieSelectionneeEntity, MediaSelectionneDto> serieMapper, Mapper<SerieSelectionneeEntity, MediaSelectionneCompletDto> serieCompletMapper) {
+    GenreMapper genreMapper;
+
+    private FilmSelectionneService filmSelectionneService;
+    private SerieSelectionneeService serieSelectionneeService;
+
+    public MediaSelectionneController(FilmSelectionneService filmSelectionneService, SerieSelectionneeService serieSelectionneeService, Mapper<FilmSelectionneEntity, MediaSelectionneDto> filmMapper, Mapper<FilmSelectionneEntity, MediaSelectionneCompletDto> filmCompletMapper, Mapper<SerieSelectionneeEntity, MediaSelectionneDto> serieMapper, Mapper<SerieSelectionneeEntity, MediaSelectionneCompletDto> serieCompletMapper, GenreMapper genreMapper) {
         this.filmSelectionneService = filmSelectionneService;
         this.serieSelectionneeService = serieSelectionneeService;
         this.filmMapper = filmMapper;
         this.filmCompletMapper = filmCompletMapper;
         this.serieMapper = serieMapper;
         this.serieCompletMapper = serieCompletMapper;
+        this.genreMapper = genreMapper;
     }
 
     @PostMapping()
     public ResponseEntity<String> creerMediaSelectionne(@RequestBody MediaSelectionneDto mediaSelectionneDto) {
-        if(mediaSelectionneDto.getTypeMedia()== TypeMedia.FILM) {
+        if (mediaSelectionneDto.getTypeMedia() == TypeMedia.FILM) {
             this.filmSelectionneService.creerFilmSelectionne(this.filmMapper.mapDtoToEntity(mediaSelectionneDto));
-        }
-        else if (mediaSelectionneDto.getTypeMedia()== TypeMedia.SERIE) {
+        } else if (mediaSelectionneDto.getTypeMedia() == TypeMedia.SERIE) {
             this.serieSelectionneeService.creerSerieSelectionnee(this.serieMapper.mapDtoToEntity(mediaSelectionneDto));
         }
 
@@ -57,10 +64,36 @@ public class MediaSelectionneController {
                         .trouverSeriesSelectionneesParEmailUtilisateur(email));
 
         List<MediaSelectionneCompletDto> mediaDtoList = new ArrayList<>();
-        filmSelectionne.stream().forEach(f->mediaDtoList.add(f));
-        serieSelectionnee.stream().forEach(s->mediaDtoList.add(s));
+        filmSelectionne.stream().forEach(f -> mediaDtoList.add(f));
+        serieSelectionnee.stream().forEach(s -> mediaDtoList.add(s));
 
         return mediaDtoList;
+    }
+
+    @GetMapping("/utilisateur/{email}/genres")
+    public TreeSet<GenreDto> trouverTousLesGenresParUtilisateur(@PathVariable("email") String email) {
+        List<FilmSelectionneEntity> filmSelectionne = this.filmSelectionneService
+                .trouverFilmsSelectionnesParEmailUtilisateur(email);
+        List<SerieSelectionneeEntity> serieSelectionnee = this.serieSelectionneeService
+                .trouverSeriesSelectionneesParEmailUtilisateur(email);
+
+        List<GenreDto> genreDtoList = new ArrayList<>();
+        filmSelectionne.forEach(f -> f.getMediaAudioVisuelEntity().getGenreList().forEach(genreEntity -> genreDtoList.add(genreMapper.mapEntityToDto(genreEntity))));
+        serieSelectionnee.forEach(s -> s.getMediaAudioVisuelEntity().getGenreList().forEach(genreEntity -> genreDtoList.add(genreMapper.mapEntityToDto(genreEntity))));
+
+        TreeSet<GenreDto> genreSet = new TreeSet<>((genre1, genre2) -> {
+            if (genre1 == null || genre2 == null) return 1;
+            if (genre1.getIdTmdb().equals(genre2.getIdTmdb()))
+                return 0; // même id TMDB = identique
+            else {
+                int compare = genre1.getNomGenre().compareTo(genre2.getNomGenre());
+                if (compare < 0) return -1;
+                else if (compare > 0) return 1;
+                else return 1; // on veut garder les doublons s'ils ont un id TMDB différents.
+            }
+        });
+        genreSet.addAll(genreDtoList);
+        return genreSet;
     }
 
     @GetMapping("/utilisateur/{email}/idtmdb/{idTmdb}")
@@ -86,13 +119,13 @@ public class MediaSelectionneController {
                                                                        @PathVariable("idTmdb") String idTmdb) {
         System.out.println("suppression");
         List<FilmSelectionneEntity> films = this.filmSelectionneService.trouverFilmSelectionnesParEmailUtilisateurEtIdTmdb(email, idTmdb);
-        if(films.size() > 0) {
+        if (films.size() > 0) {
             this.filmSelectionneService.supprimerFilmSelectionneParId(films.get(0).getId());
             return new ResponseEntity<>("Media sélectionné supprimé", HttpStatus.ACCEPTED);
         }
 
         List<SerieSelectionneeEntity> series = this.serieSelectionneeService.trouverSeriesSelectionneesParEmailUtilisateurEtIdTmdb(email, idTmdb);
-        if(series.size() > 0) {
+        if (series.size() > 0) {
             this.serieSelectionneeService.supprimerSerieSelectionneeParId(series.get(0).getId());
             return new ResponseEntity<>("Media sélectionné supprimé", HttpStatus.ACCEPTED);
         }
