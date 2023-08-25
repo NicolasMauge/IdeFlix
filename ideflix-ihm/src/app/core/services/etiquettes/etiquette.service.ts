087b6083d97b0e23d0c08e341238cc3e0cc50791
Caroline Lambert <caroline.lambert@epita.fr>
2023-08-25 15:21:15 +0200

    IHM - affichage saison et épisode sélectionnés pour une série

--- ideflix-ihm/src/app/core/services/etiquettes/etiquette.service.ts
+++ ideflix-ihm/src/app/core/services/etiquettes/etiquette.service.ts
@@ -40,10 +40,12 @@ export class EtiquetteService {
       );
   }
 
-  saveToApp(etiquette: EtiquetteModel) {
-    let endpoint = '/etiquette';
+  saveToApp(etiquette: EtiquetteModel, email: string): Observable<string> {
+    /*console.log("sauve étiquette");
+    console.log(etiquette);
+    console.log(email);*/
+    let endpoint = '/etiquette/utilisateur/'+email;
 
-    return this.http
-      .post(this.IDEFLIX_API + endpoint, etiquette, {responseType: 'text'});
+    return this.http.post(this.IDEFLIX_API + endpoint, etiquette, {responseType: 'text'});
   }
 }
