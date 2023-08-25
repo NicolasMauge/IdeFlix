0f42ac785901b9a21d7f78dcfbcef3939b35d857
Stéphane Eveillé <stephane.eveille-epita@outlook.fr>
2023-08-25 17:05:46 +0200

    APP : ajout du lien vers le swagger dans la log au démarrage.

--- ideflix-app/exposition/src/main/java/org/epita/StarterMain.java
+++ ideflix-app/exposition/src/main/java/org/epita/StarterMain.java
@@ -1,11 +1,21 @@
 package org.epita;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StarterMain {
+
+    public static final Logger logger = LoggerFactory.getLogger(StarterMain.class);
+
     public static void main(String[] args) {
         SpringApplication.run(StarterMain.class, args);
+
+        logger.info("==========================================================");
+        logger.info("= Démarrage Ideflix APP                                  =");
+        logger.info("= http://localhost:8081/api/v1/swagger-ui/               =");
+        logger.info("==========================================================");
     }
 }
