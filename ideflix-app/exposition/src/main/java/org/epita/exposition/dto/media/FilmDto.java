2c91411ead6c4a7db8eb8477723416175c7d034b
Stéphane Eveillé <stephane.eveille-epita@outlook.fr>
2023-08-25 15:21:29 +0200

    APP : documentation swagger

--- ideflix-app/exposition/src/main/java/org/epita/exposition/dto/media/FilmDto.java
+++ ideflix-app/exposition/src/main/java/org/epita/exposition/dto/media/FilmDto.java
@@ -1,16 +1,29 @@
 package org.epita.exposition.dto.media;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDate;
 import java.util.List;
 
 public class FilmDto {
+    @Schema(name = "Identifiant TMDB du film", example = "603")
     private String idTmdb;
+    @Schema(name = "Titre du film", example = "Matrix")
     private String titre;
+    @Schema(name = "Date de sortie du film", example = "1999-03-30")
     private LocalDate dateSortie;
+    @Schema(name = "Durée en minutes", example = "135")
     private int duree;
+
+    @Schema(name = "Chemin de l'affiche dans TMDB au format portrait", example = "/pEoqbqtLc4CcwDUDqxmEDSWpWTZ.jpg")
     private String cheminAffichePortrait;
+    @Schema(name = "Chemin de l'affiche dans TMDB au format paysage", example = "/oMsxZEvz9a708d49b6UdZK1KAo5.jpg")
     private String cheminAffichePaysage;
+
+    @Schema(name = "Score TMDB", example = "8")
     private int noteTmdb;
+
+    @Schema(name = "Liste des genres du film")
     private List<GenreDto> genreList;
 
     public FilmDto() {
