e7101537f4c24569692cfd0da72cca6a8d474f3c
Stéphane Eveillé <stephane.eveille-epita@outlook.fr>
2023-08-27 12:01:28 +0200

    IHM : création des composants core : chargement-en-cours et chargement-suite

---
+++ ideflix-ihm/src/app/core/components/chargement-suite/chargement-suite.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chargement-suite',
+  templateUrl: './chargement-suite.component.html',
+  styleUrls: ['./chargement-suite.component.css']
+})
+export class ChargementSuiteComponent {
+
+}
--- ideflix-ihm/src/app/core/core.module.ts
+++ ideflix-ihm/src/app/core/core.module.ts
@@ -7,11 +7,15 @@ import {NavbarComponent} from "./components/navbar/navbar.component";
 import {RouterModule} from "@angular/router";
 import {MatSnackBarModule} from "@angular/material/snack-bar";
 import {StarComponent} from "./components/star/star.component";
+import {ChargementEnCoursComponent} from './components/chargement-en-cours/chargement-en-cours.component';
+import {ChargementSuiteComponent} from './components/chargement-suite/chargement-suite.component';
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    StarComponent
+    StarComponent,
+    ChargementEnCoursComponent,
+    ChargementSuiteComponent
   ],
   imports: [
     CommonModule,
@@ -21,7 +25,9 @@ import {StarComponent} from "./components/star/star.component";
   ],
   exports: [
     NavbarComponent,
-    StarComponent
+    StarComponent,
+    ChargementEnCoursComponent,
+    ChargementSuiteComponent
   ],
 
   providers: [
