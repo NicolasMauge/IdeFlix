2adaeebe3fd222f9f6582deae105a1c3f9914130
Stéphane Eveillé <stephane.eveille-epita@outlook.fr>
2023-08-27 15:03:53 +0200

    IHM : ajout chargement-en-cours en cours dans plusieurs pages

--- ideflix-ihm/src/app/core/components/chargement-en-cours/chargement-en-cours.component.ts
+++ ideflix-ihm/src/app/core/components/chargement-en-cours/chargement-en-cours.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-chargement-en-cours',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./chargement-en-cours.component.css']
 })
 export class ChargementEnCoursComponent {
-
+  @Input() message!: string;
 }
