911ea74ef387ba84c1a82d91f6a6239944d2e0e9
Caroline Lambert <caroline.lambert@epita.fr>
2023-08-28 16:05:29 +0200

    APP - amélioration @Tests

--- ideflix-app/application/src/test/java/org/epita/application/mediaDataBase/genreDataBase/GenreDataBaseServiceTest.java
+++ ideflix-app/application/src/test/java/org/epita/application/mediaDataBase/genreDataBase/GenreDataBaseServiceTest.java
@@ -1,38 +1,69 @@
 package org.epita.application.mediaDataBase.genreDataBase;
 
 import org.epita.domaine.media.GenreEntity;
-import org.epita.domaine.mediaDataBase.GenreDataBase;
-import org.epita.infrastructure.mediaDataBase.GenreDataBaseRepositoryImpl;
-import org.epita.infrastructure.mediaDataBase.TmdbConfig;
-import org.epita.infrastructure.mediaDataBase.mapper.GenreApiMapper;
+import org.epita.infrastructure.mediaDataBase.GenreDataBaseRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = {GenreDataBaseServiceImpl.class, GenreDataBaseRepositoryImpl.class, TmdbConfig.class, GenreApiMapper.class})
+@SpringBootTest(classes = {GenreDataBaseServiceImpl.class})
 public class GenreDataBaseServiceTest {
 
     @Autowired
     GenreDataBaseService genreDataBaseService;
 
+    @MockBean
+    GenreDataBaseRepository genreRepository;
+
+    @BeforeEach
+    public void setUp() {
+        List<GenreEntity> genreEntityListMovie = new ArrayList<>();
+        GenreEntity genre = new GenreEntity();
+        genre.setId(1L);
+        genre.setIdTmdb("1");
+        genre.setNomGenre("genre 1");
+        genreEntityListMovie.add(genre);
+
+        GenreEntity genre2 = new GenreEntity();
+        genre2.setId(2L);
+        genre2.setIdTmdb("2");
+        genre2.setNomGenre("genre 2");
+        genreEntityListMovie.add(genre2);
+
+        List<GenreEntity> genreEntityListSerie = new ArrayList<>();
+        GenreEntity genre3 = new GenreEntity();
+        genre3.setId(3L);
+        genre3.setIdTmdb("3");
+        genre3.setNomGenre("genre 3");
+        genreEntityListSerie.add(genre);
+        genreEntityListSerie.add(genre3);
+
+        when(genreRepository.searchAllGenresEntityForMovie()).thenReturn(genreEntityListMovie);
+        when(genreRepository.searchAllGenresEntityForTV()).thenReturn(genreEntityListSerie);
+    }
+
     @Test
     public void rechercherLaListeDeTousLesGenres() {
-        // Given
+        // Given  voir @BeforeEach
 
         // When
         final List<GenreEntity> trouves = this.genreDataBaseService.searchAllGenresEntity();
-        System.out.println("trouves: " + trouves);
+        System.out.println("trouves: " + trouves.get(0).getId());
 
         // Then
-        //le tableau des genres trouvés est de 27
-        assertThat(trouves).hasSize(27);
+        //le tableau des genres trouvés est de 3
+        assertThat(trouves).hasSize(3);
     }
 
 }
--- ideflix-app/application/src/test/java/org/epita/application/mediaDataBase/movieDataBase/MovieDataBaseServiceTest.java
+++ ideflix-app/application/src/test/java/org/epita/application/mediaDataBase/movieDataBase/MovieDataBaseServiceTest.java
@@ -1,7 +1,10 @@
 package org.epita.application.mediaDataBase.movieDataBase;
 
+import org.epita.application.mediaDataBase.genreDataBase.GenreDataBaseService;
+import org.epita.application.utilisateur.preferences.PreferencesUtilisateurService;
 import org.epita.domaine.mediaDataBase.GenreDataBase;
 import org.epita.domaine.mediaDataBase.MovieDataBase;
+import org.epita.infrastructure.mediaDataBase.MovieDataBaseRepository;
 import org.epita.infrastructure.mediaDataBase.MovieDataBaseRepositoryImpl;
 import org.epita.infrastructure.mediaDataBase.TmdbConfig;
 import org.epita.infrastructure.mediaDataBase.apidto.MovieLightResponseDto;
@@ -13,6 +16,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
@@ -20,14 +24,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = {MovieDataBaseServiceImpl.class, MovieDataBaseRepositoryImpl.class, TmdbConfig.class, MovieApiMapper.class, GenreApiMapper.class})
+@SpringBootTest(classes = {MovieDataBaseServiceImpl.class})
 public class MovieDataBaseServiceTest {
 
     @Autowired
     MovieDataBaseService movieDataBaseService;
 
+    @MockBean
+    MovieDataBaseRepository movieDataBaseRepositoryMock;
+
+    @MockBean
+    GenreDataBaseService genreDataBaseServiceMock;
+
+    @MockBean
+    PreferencesUtilisateurService preferencesUtilisateurService;
+
+    private MovieDataBase movie1;
 
     @BeforeEach
     public void setUp() {
@@ -51,105 +66,38 @@ public class MovieDataBaseServiceTest {
         List<MovieDataBase> listMovies = new ArrayList<>();
         listMovies.add(movie1);
 
-    }
+        when(movieDataBaseRepositoryMock.findDetailMovieDataBase(8265L)).thenReturn(movie1);
 
-
-    @Test
-    public void rechercherLaListeDeTousLesFilms_contenant_caracteres_bienvenuechezleschtis() {
-        // Given
-        String query = "bienvenue%20chez%20les%20ch%27tis";
-
-        // When
-        final List<MovieDataBase> trouves = this.movieDataBaseService.searchMovies(query);
-        System.out.println("trouves: " + trouves);
-
-        // Then
-        //le tableau des films trouvés est de 1
-        assertThat(trouves).hasSize(1);
-    }
-
-    @Test
-    public void rechercherLaListeDeTousLesFilms_contenant_caracteres_marsattack() {
-        // Given
-        String query = "mars%20attack";
-
-        // When
-        final List<MovieDataBase> trouves = this.movieDataBaseService.searchMovies(query);
-        System.out.println("trouves: " + trouves);
-
-        // Then
-        //le tableau des films trouvés est de 1
-        assertThat(trouves).hasSize(5);
-    }
-
-    @Test
-    public void rechercherLaListeDeTousLesFilms_contenant_caracteres_bienvenuechezleschtis_appel_API() {
-        // Given
-        String query = "bienvenue%20chez%20les%20ch%27tis";
-
-        SearchMoviesResponseDto movie1 = new SearchMoviesResponseDto();
-        movie1.setPage(1);
-
-        MovieLightResponseDto movieLightResponseDto = new MovieLightResponseDto();
-        movieLightResponseDto.setAdult(false);
-        movieLightResponseDto.setBackdrop_path("/sBmLZreEkVK3bc6LLEyZFgKuxjk.jpg");
-        List<Integer> genres = new ArrayList<>();
-        genres.add(35);
-        genres.add(18);
-        genres.add(10749);
-        movieLightResponseDto.setGenre_ids(genres);
-        movieLightResponseDto.setId(8265);
-        movieLightResponseDto.setOriginal_language("fr");
-        movieLightResponseDto.setOriginal_title("Bienvenue chez les Ch'tis");
-        movieLightResponseDto.setOverview("Philippe Abrams est directeur de la poste de Salon‐de‐Provence. Il est marié à Julie, dont le caractère dépressif lui rend la vie impossible. Pour lui faire plaisir, Philippe fraude afin d’obtenir une mutation sur la Côte d’Azur. Mais il est démasqué: il sera muté à Bergues, petite ville du Nord. Pour les Abrams, sudistes pleins de préjugés, le Nord c’est l’horreur, une région glacée, peuplée d’êtres rustres, éructant un langage incompréhensible, le «cheutimi». Philippe ira seul. À sa grande surprise, il découvre un endroit charmant, une équipe chaleureuse, des gens accueillants, et se fait un ami: Antoine, le facteur et le carillonneur du village, à la mère possessive et aux amours contrariées. Quand Philippe revient à Salon, Julie refuse de croire qu’il se plait dans le Nord. Elle pense même qu’il lui ment pour la ménager. Pour la satisfaire et se simplifier la vie, Philippe lui fait croire qu’en effet, il vit un enfer à Bergues.");
-        movieLightResponseDto.setPopularity(15.869f);
-        movieLightResponseDto.setPoster_path("/dfht1lGq2ALbrRkMj35dUrj5kHG.jpg");
-        movieLightResponseDto.setRelease_date("2008-02-20");
-        movieLightResponseDto.setTitle("Bienvenue chez les Ch'tis");
-        movieLightResponseDto.setVideo(false);
-        movieLightResponseDto.setVote_average(6.672f);
-        movieLightResponseDto.setVote_count(2264);
-
-        List<MovieLightResponseDto> results = new ArrayList<>();
-        results.add(movieLightResponseDto);
-        movie1.setResults(results);
-        movie1.setTotal_pages(1);
-        movie1.setTotal_results(1);
-
-        // When
-        final List<MovieDataBase> trouves = this.movieDataBaseService.searchMovies(query);
-        System.out.println("movie1: " + movie1);
-        System.out.println("trouves: " + trouves);
-
-        // Then
-        // comparaison de l'id du film trouvé en index 1
-        assertThat(trouves.get(0).getIdDataBase()).isEqualTo(movie1.getResults().get(0).getId());
     }
 
     @Test
     public void rechercherDetailDuFilm_IdTmdb_8265() {
 
+        // Given  @BeforeEach
+        long Id = 8265;
+
         MovieDataBase movie1 = new MovieDataBase();
         movie1.setIdDataBase(8265L);
-        movie1.setDateSortie(LocalDate.of(2008, 2, 20));
-        movie1.setDuree(107);
-        movie1.setNoteDataBase(6.672F);
+        movie1.setDateSortie(LocalDate.of(2008,2,20));
+        movie1.setDuree(0);
+        movie1.setNoteDataBase(6.673F);
         movie1.setTitre("Bienvenue chez les Ch'tis");
         movie1.setCheminAffichePaysage("/sBmLZreEkVK3bc6LLEyZFgKuxjk.jpg");
         movie1.setCheminAffichePortrait("/dfht1lGq2ALbrRkMj35dUrj5kHG.jpg");
         movie1.setResume("Philippe Abrams est directeur de la poste de Salon‐de‐Provence. Il est marié à Julie, dont le caractère dépressif lui rend la vie impossible. Pour lui faire plaisir, Philippe fraude afin d’obtenir une mutation sur la Côte d’Azur. Mais il est démasqué: il sera muté à Bergues, petite ville du Nord. Pour les Abrams, sudistes pleins de préjugés, le Nord c’est l’horreur, une région glacée, peuplée d’êtres rustres, éructant un langage incompréhensible, le «cheutimi». Philippe ira seul. À sa grande surprise, il découvre un endroit charmant, une équipe chaleureuse, des gens accueillants, et se fait un ami: Antoine, le facteur et le carillonneur du village, à la mère possessive et aux amours contrariées. Quand Philippe revient à Salon, Julie refuse de croire qu’il se plait dans le Nord. Elle pense même qu’il lui ment pour la ménager. Pour la satisfaire et se simplifier la vie, Philippe lui fait croire qu’en effet, il vit un enfer à Bergues.");
         List<GenreDataBase> genresMovie = new ArrayList<>();
-        genresMovie.add(0, new GenreDataBase(35, "Comédie"));
+        genresMovie.add(0, new GenreDataBase(35, "Comedie"));
         genresMovie.add(1, new GenreDataBase(18, "Drame"));
         genresMovie.add(2, new GenreDataBase(10749, "Romance"));
         movie1.setGenres(genresMovie);
-        System.out.println("movie1: " + movie1);
 
-        // Given
-        long Id = 8265;
+        List<MovieDataBase> listMovies = new ArrayList<>();
+        listMovies.add(movie1);
+
+        when(movieDataBaseRepositoryMock.findDetailMovieDataBase(8265L)).thenReturn(movie1);
 
         // When
-        final MovieDataBase filmTrouve = this.movieDataBaseService.findMovieById(8265);
+        final MovieDataBase filmTrouve = this.movieDataBaseService.findMovieById(Id);
 
         // Then
         //comparaison du titre du film trouvé
@@ -157,18 +105,93 @@ public class MovieDataBaseServiceTest {
         assertThat(filmTrouve.getTitre()).isEqualTo(movie1.getTitre());
     }
 
-    @Test
-    public void rechercherSuggestionDeFilms_Pour_Page_1_appel_API() {
-        // Given
-        int page = 1;
-
-        // When
-        final List<MovieDataBase> trouves = this.movieDataBaseService.searchSuggestedMovies(page);
-
-        // Then
-
-        //le tableau des films trouvés est de 20
-        assertThat(trouves).hasSize(20);
-
-    }
+//    @Test
+//    public void rechercherLaListeDeTousLesFilms_contenant_caracteres_bienvenuechezleschtis() {
+//        // Given
+//        String query = "bienvenue%20chez%20les%20ch%27tis";
+//        final MovieDataBase expected = this.movieDataBaseRepositoryMock.searchAllMovieDataBaseWithQuery(query);
+//
+//        // When
+//        final List<MovieDataBase> trouves = this.movieDataBaseService.searchMovies(query);
+//        System.out.println("trouves: " + trouves);
+//
+//        // Then
+//        //le tableau des films trouvés est de 1
+//        assertThat(trouves).hasSize(1);
+//    }
+//
+//    @Test
+//    public void rechercherLaListeDeTousLesFilms_contenant_caracteres_marsattack() {
+//        // Given
+//        String query = "mars%20attack";
+//
+//        // When
+//        final List<MovieDataBase> trouves = this.movieDataBaseService.searchMovies(query);
+//        System.out.println("trouves: " + trouves);
+//
+//        // Then
+//        //le tableau des films trouvés est de 1
+//        assertThat(trouves).hasSize(5);
+//    }
+//
+//    @Test
+//    public void rechercherLaListeDeTousLesFilms_contenant_caracteres_bienvenuechezleschtis_appel_API() {
+//        // Given
+//        String query = "bienvenue%20chez%20les%20ch%27tis";
+//
+//        SearchMoviesResponseDto movie1 = new SearchMoviesResponseDto();
+//        movie1.setPage(1);
+//
+//        MovieLightResponseDto movieLightResponseDto = new MovieLightResponseDto();
+//        movieLightResponseDto.setAdult(false);
+//        movieLightResponseDto.setBackdrop_path("/sBmLZreEkVK3bc6LLEyZFgKuxjk.jpg");
+//        List<Integer> genres = new ArrayList<>();
+//        genres.add(35);
+//        genres.add(18);
+//        genres.add(10749);
+//        movieLightResponseDto.setGenre_ids(genres);
+//        movieLightResponseDto.setId(8265);
+//        movieLightResponseDto.setOriginal_language("fr");
+//        movieLightResponseDto.setOriginal_title("Bienvenue chez les Ch'tis");
+//        movieLightResponseDto.setOverview("Philippe Abrams est directeur de la poste de Salon‐de‐Provence. Il est marié à Julie, dont le caractère dépressif lui rend la vie impossible. Pour lui faire plaisir, Philippe fraude afin d’obtenir une mutation sur la Côte d’Azur. Mais il est démasqué: il sera muté à Bergues, petite ville du Nord. Pour les Abrams, sudistes pleins de préjugés, le Nord c’est l’horreur, une région glacée, peuplée d’êtres rustres, éructant un langage incompréhensible, le «cheutimi». Philippe ira seul. À sa grande surprise, il découvre un endroit charmant, une équipe chaleureuse, des gens accueillants, et se fait un ami: Antoine, le facteur et le carillonneur du village, à la mère possessive et aux amours contrariées. Quand Philippe revient à Salon, Julie refuse de croire qu’il se plait dans le Nord. Elle pense même qu’il lui ment pour la ménager. Pour la satisfaire et se simplifier la vie, Philippe lui fait croire qu’en effet, il vit un enfer à Bergues.");
+//        movieLightResponseDto.setPopularity(15.869f);
+//        movieLightResponseDto.setPoster_path("/dfht1lGq2ALbrRkMj35dUrj5kHG.jpg");
+//        movieLightResponseDto.setRelease_date("2008-02-20");
+//        movieLightResponseDto.setTitle("Bienvenue chez les Ch'tis");
+//        movieLightResponseDto.setVideo(false);
+//        movieLightResponseDto.setVote_average(6.672f);
+//        movieLightResponseDto.setVote_count(2264);
+//
+//        List<MovieLightResponseDto> results = new ArrayList<>();
+//        results.add(movieLightResponseDto);
+//        movie1.setResults(results);
+//        movie1.setTotal_pages(1);
+//        movie1.setTotal_results(1);
+//
+//        // When
+//        final List<MovieDataBase> trouves = this.movieDataBaseService.searchMovies(query);
+//        System.out.println("movie1: " + movie1);
+//        System.out.println("trouves: " + trouves);
+//
+//        // Then
+//        // comparaison de l'id du film trouvé en index 1
+//        assertThat(trouves.get(0).getIdDataBase()).isEqualTo(movie1.getResults().get(0).getId());
+//    }
+//
+//
+//
+//    @Test
+//    public void rechercherSuggestionDeFilms_Pour_Page_1_appel_API() {
+//        // Given
+//        int page = 1;
+//
+//        // When
+//        final List<MovieDataBase> trouves = this.movieDataBaseService.searchSuggestedMovies(page);
+//
+//        // Then
+//
+//        //le tableau des films trouvés est de 20
+//        assertThat(trouves).hasSize(20);
+//
+//    }
 }
