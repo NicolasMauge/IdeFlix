5c9dcbaee88e1af94878dee4a0f267e0fccb9eaf
Stéphane Eveillé <stephane.eveille-epita@outlook.fr>
2023-08-29 13:51:34 +0200

    APP : Javadoc à compléter

--- ideflix-app/exposition/src/main/java/org/epita/StarterMain.java
+++ ideflix-app/exposition/src/main/java/org/epita/StarterMain.java
@@ -5,11 +5,21 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Classe de démarrage de l'application Springboot.
+ */
+
 @SpringBootApplication
 public class StarterMain {
 
     public static final Logger logger = LoggerFactory.getLogger(StarterMain.class);
 
+
+    /**
+     * Méthode pour le lancement de l'application.
+     *
+     * @param args Paramètres fournis au lancement (dans la ligne de commande). Les paramètres ne sont pas utilisés et sont ignorés.
+     */
     public static void main(String[] args) {
         SpringApplication.run(StarterMain.class, args);
 
@@ -17,5 +27,6 @@ public class StarterMain {
         logger.info("= Démarrage Ideflix APP                                  =");
         logger.info("= http://localhost:8081/api/v1/swagger-ui.html           =");
         logger.info("==========================================================");
+
     }
 }
